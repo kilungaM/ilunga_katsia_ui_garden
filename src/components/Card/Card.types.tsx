8167342b17ae5597cf9ed0377337556c6183d8a0
kilungaM <kilunga@Academic.RRC.CA>
2025-11-05 12:03:12 -0600

    Final working Husky pre-commit and CI workflow

--- src/components/Card/Card.types.tsx
+++
@@ -1,9 +0,0 @@
-export interface CardProps {
-  children: React.ReactNode;
-  disabled?: boolean;
-  backgroundColor?: string;
-  padding?: string;
-  shadow?: boolean;
-  borderRadius?: string;
-  className?: string;
-}
